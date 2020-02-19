7d54e972d6b622f157be753b4147ae55e591ee52
Artem Frolov <57588157+artemf92@users.noreply.github.com>
2020-02-19 14:47:11 +0200

    add sweipers for mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
 $(document).ready(function() {
 
 	
-	/* Call plugins hrere ...*/
+var swiperBlocks = new Swiper('.swiper-container', {
+    speed: 2000,
+    spaceBetween: 100,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    // breakpoints: {
+    // 	320: {
+    // 		slidesPerView: 1
+    // 	},
+    // 	720: {
+    // 		slidesPerView: 2
+    // 	}
+    // }
+});
 
+// var swiperProjects = new Swiper('.swiper-container', {
+//     speed: 2000,
+//     spaceBetween: 100,
+//     pagination: {
+//       el: '.swiper-pagination__two',
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next__two',
+//       prevEl: '.swiper-button-prev__two',
+//     },
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     }
+// });
 	
 });
